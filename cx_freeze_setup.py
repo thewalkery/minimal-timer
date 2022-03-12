30d6c4b13ef53a99b9310a3940f6608eeaa79128
thewalkery <thewalkery74@gmail.com>
2022-03-12 22:09:41 +0900

    Changed cx_freeze_setup.py

--- cx_freeze_setup.py
+++ cx_freeze_setup.py
@@ -15,5 +15,5 @@ setup(
     version="1.0",
     description="Minimal timer",
     options={"build_exe": build_exe_options},
-    executables=[Executable("run.py", base=base)],
+    executables=[Executable("minimaltimer/app.py", base=base)],
 )
